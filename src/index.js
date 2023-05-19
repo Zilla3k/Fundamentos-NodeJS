37be4a441ec86169565f72a8f63baa892e5c2861
zilla3k <henriquepelanda.web@gmail.com>
2023-05-18 22:39:55 -0300

    criando cliente

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 const express = require('express');
+const { v4: uuidV4 } = require('uuid');
 
 const app = express();
+
+app.use(express.json());
+
+const clientes = [];
+
+/**
+ * CPF - string
+ * Nome - string
+ * id - uuidV4
+ * Extrato - []
+ */
+app.post('/conta', (req, res) => {
+  const { cpf, nome } = req.body;
+  const id = uuidV4();
+
+  clientes.push({
+    cpf,
+    nome,
+    id,
+    extrato: [],
+  });
+
+  return res.status(201).send();
+});
 
 app.listen(3333);
